7cc799a2fe06a7ef2d647712ea8cd5b4558d8fba
Wizacass <visak.pet0@gmail.com>
2021-09-16 14:41:50 +0300

    Add testing framework

---
+++ Saitynas_API_Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace Saitynas_API_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
