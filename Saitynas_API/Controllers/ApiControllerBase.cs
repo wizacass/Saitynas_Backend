8e08bcd34358b85f4e918806fb16d5a5c1e97fbd
Wizacass <visak.pet0@gmail.com>
2021-09-26 02:11:08 +0300

    Create Workplace controller stub

--- Saitynas_API/Controllers/ApiControllerBase.cs
+++ Saitynas_API/Controllers/ApiControllerBase.cs
@@ -1,18 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using Saitynas_API.Models;
+using Saitynas_API.Models.Common;
+using Saitynas_API.Models.DTO.Common;
 
 namespace Saitynas_API.Controllers
 {
-    public class ApiControllerBase : ControllerBase
+    public abstract class ApiControllerBase : ControllerBase
     {
         protected const string ApiContentType = "application/json";
         protected const string RoutePrefix = "api/v1";
 
+        protected abstract string ModelName { get; }
+
         protected ApiContext Context { get; }
-        
-        public ApiControllerBase(ApiContext context)
+
+        protected ApiControllerBase(ApiContext context)
         {
             Context = context;
+        }
+        
+        protected ActionResult ApiCreated(object data)
+        {
+            return StatusCode(201, data);
+        }
+        
+        protected NotFoundObjectResult ApiNotFound()
+        {
+            var error = new ErrorDTO
+            {
+                Type = 404,
+                Title = ApiErrorSlug.ResourceNotFound,
+                Details = ModelName
+            };
+            return NotFound(error);
+        }
+
+        protected NotFoundObjectResult ApiNotFound(string title, string details = null)
+        {
+            var error = new ErrorDTO
+            {
+                Type = 404,
+                Title = title,
+                Details = details
+            };
+            return NotFound(error);
         }
     }
 }
---
+++ Saitynas_API/Models/Workplaces/DTO/WorkplaceDTO.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json;
+
+namespace Saitynas_API.Models.Workplaces.DTO
+{
+    public class WorkplaceDTO
+    {
+        [JsonProperty("address")]
+        public string Address { get; set; }
+        
+        [JsonProperty("city")]
+        public string City { get; set; }
+    }
+}
