d999748534e085143aa7745e6922cd1035860c65
Wizacass <visak.pet0@gmail.com>
2021-09-28 19:13:18 +0300

    Update migrations

---
+++ Saitynas_API/Migrations/20210928160844_AddWorkplaces.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace Saitynas_API.Migrations
+{
+    public partial class AddWorkplaces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Workplaces",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    City = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workplaces", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workplaces");
+        }
+    }
+}
