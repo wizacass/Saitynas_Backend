78a8649c68d0df402b52f03020870a474d08bd4c
Wizacass <visak.pet0@gmail.com>
2021-11-02 17:10:31 +0200

    Implement logic to retrieve all users

--- Saitynas_API/Controllers/UsersController.cs
+++ Saitynas_API/Controllers/UsersController.cs
@@ -1,11 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Saitynas_API.Models.Common;
 using Saitynas_API.Models.DTO;
+using Saitynas_API.Models.Entities.Evaluation;
+using Saitynas_API.Models.Entities.Evaluation.DTO;
+using Saitynas_API.Models.Entities.Role;
 using Saitynas_API.Models.Entities.User;
 using Saitynas_API.Models.Entities.User.DTO;
+using Saitynas_API.Repositories;
+using Saitynas_API.Services;
 
 namespace Saitynas_API.Controllers
 {
@@ -16,7 +24,30 @@ namespace Saitynas_API.Controllers
     {
         protected override string ModelName => "user";
 
-        public UsersController(UserManager<User> userManager) : base(userManager) { }
+        private readonly IApiUserStore _userStore;
+        private readonly IEvaluationsRepository _evaluationsRepository;
+
+        public UsersController(
+            IApiUserStore userStore, 
+            IEvaluationsRepository evaluationsRepository,
+            UserManager<User> userManager
+            ) : base(userManager)
+        {
+            _userStore = userStore;
+            _evaluationsRepository = evaluationsRepository;
+        }
+        
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<GetListDTO<UserDTO>>> GetUsers()
+        {
+            var users = (await _userStore.GetAllUsers())
+                .Select(u => new UserDTO(u));
+
+            var dto = new GetListDTO<UserDTO>(users);
+          
+            return Ok(dto);
+        }
 
         [HttpGet("me")]
         [Authorize(Roles = AllRoles)]
