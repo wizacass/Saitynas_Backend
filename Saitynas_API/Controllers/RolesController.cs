8026609260fb3930eb29ff4c8d966836206da56d
wizacass <visak.pet0@gmail.com>
2021-11-24 22:00:41 +0200

    Implement logic to retrieve roles

---
+++ Saitynas_API/Controllers/RolesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Saitynas_API.Models.DTO;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Saitynas_API.Models.Entities.Role;
+
+namespace Saitynas_API.Controllers
+{
+    [Route(RoutePrefix + "/[Controller]")]
+    [ApiController]
+    public class RolesController : ApiControllerBase
+    {
+        protected override string ModelName => "Role";
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult<GetListDTO<GetEnumDTO>> GetRoles()
+        {
+            var roles = Enum.GetValues(typeof(RoleId))
+                .Cast<RoleId>()
+                .Skip(2)
+                .Select(r => new GetEnumDTO
+                {
+                    Id = (int)r,
+                    Name = r.ToString()
+                })
+                .ToList();
+
+            var dto = new GetListDTO<GetEnumDTO>(roles);
+
+            return Ok(dto);
+        }
+    }
+}
