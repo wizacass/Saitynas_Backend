a746c4717e9cab4c8b404effa9cfc706a85f1c8d
Vytenis Petrauskas <visak.pet0@gmail.com>
2022-01-16 20:12:33 +0200

    Apply file-scoped namespaces to repositories

--- Saitynas_API/Repositories/IRepository.cs
+++ Saitynas_API/Repositories/IRepository.cs
@@ -1,18 +1,17 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Saitynas_API.Repositories
+namespace Saitynas_API.Repositories;
+
+public interface IRepository<T>
 {
-    public interface IRepository<T>
-    {
-        Task<IEnumerable<T>> GetAllAsync();
-        
-        Task<T> GetAsync(int id);
-        
-        Task InsertAsync(T data);
-        
-        Task UpdateAsync(int id, T data);
-        
-        Task DeleteAsync(int id);
-    }
+    Task<IEnumerable<T>> GetAllAsync();
+
+    Task<T> GetAsync(int id);
+
+    Task InsertAsync(T data);
+
+    Task UpdateAsync(int id, T data);
+
+    Task DeleteAsync(int id);
 }
