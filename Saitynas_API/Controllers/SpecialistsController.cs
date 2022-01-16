bae19077304f83c73e289247c932a599eea04fbb
Vytenis Petrauskas <visak.pet0@gmail.com>
2022-01-16 20:27:23 +0200

    Apply file-scoped namespaces to controllers

--- Saitynas_API/Controllers/SpecialistsController.cs
+++ Saitynas_API/Controllers/SpecialistsController.cs
@@ -12,124 +12,123 @@ using Saitynas_API.Models.Entities.Visit.DTO;
 using Saitynas_API.Repositories;
 using Saitynas_API.Services.Validators;
 
-namespace Saitynas_API.Controllers
+namespace Saitynas_API.Controllers;
+
+[Route(RoutePrefix + "/[Controller]")]
+[ApiController]
+[Produces(ApiContentType)]
+public class SpecialistsController : ApiControllerBase
 {
-    [Route(RoutePrefix + "/[Controller]")]
-    [ApiController]
-    [Produces(ApiContentType)]
-    public class SpecialistsController : ApiControllerBase
+    private readonly IEvaluationsRepository _evaluationsRepository;
+
+    private readonly ISpecialistsRepository _specialistsRepository;
+    private readonly ISpecialistDTOValidator _validator;
+    protected override string ModelName => "specialist";
+
+    public SpecialistsController(
+        ISpecialistsRepository specialistsRepository,
+        IEvaluationsRepository evaluationsRepository,
+        ISpecialistDTOValidator validator
+    )
     {
-        protected override string ModelName => "specialist";
+        _specialistsRepository = specialistsRepository;
+        _evaluationsRepository = evaluationsRepository;
+        _validator = validator;
+    }
 
-        private readonly ISpecialistsRepository _specialistsRepository;
-        private readonly IEvaluationsRepository _evaluationsRepository;
-        private readonly ISpecialistDTOValidator _validator;
+    [HttpGet]
+    [Authorize(Roles = "Admin,Patient")]
+    public async Task<ActionResult<GetListDTO<GetSpecialistDTO>>> GetSpecialists()
+    {
+        var specialists = (await _specialistsRepository.GetAllAsync())
+            .Select(w => new GetSpecialistDTO(w));
 
-        public SpecialistsController(
-            ISpecialistsRepository specialistsRepository,
-            IEvaluationsRepository evaluationsRepository,
-            ISpecialistDTOValidator validator
-        )
+        var dto = new GetListDTO<GetSpecialistDTO>(specialists);
+
+        return Ok(dto);
+    }
+
+    [HttpGet("{id:int}")]
+    [Authorize(Roles = "Admin,Patient")]
+    public async Task<ActionResult<GetObjectDTO<GetSpecialistDTO>>> GetSpecialist(int id)
+    {
+        var specialist = await _specialistsRepository.GetAsync(id);
+
+        if (specialist == null) return ApiNotFound();
+
+        var dto = new GetSpecialistDTO(specialist);
+        return Ok(new GetObjectDTO<GetSpecialistDTO>(dto));
+    }
+
+    [HttpGet("{id:int}/evaluations")]
+    [Authorize(Roles = "Admin,Patient")]
+    public async Task<ActionResult<GetListDTO<GetEvaluationDTO>>> GetSpecialistEvaluations(int id)
+    {
+        var evaluations = (await _evaluationsRepository.GetBySpecialistId(id))
+            .Select(e => new GetEvaluationDTO(e));
+
+        return Ok(new GetListDTO<GetEvaluationDTO>(evaluations));
+    }
+
+    [Obsolete("This is a mock implementation")]
+    [HttpGet("{id:int}/visits")]
+    public ActionResult<GetListDTO<GetVisitDTO>> GetSpecialistVisits(int id)
+    {
+        var evaluations = new List<GetVisitDTO>
         {
-            _specialistsRepository = specialistsRepository;
-            _evaluationsRepository = evaluationsRepository;
-            _validator = validator;
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin,Patient")]
-        public async Task<ActionResult<GetListDTO<GetSpecialistDTO>>> GetSpecialists()
-        {
-            var specialists = (await _specialistsRepository.GetAllAsync())
-                .Select(w => new GetSpecialistDTO(w));
-
-            var dto = new GetListDTO<GetSpecialistDTO>(specialists);
-
-            return Ok(dto);
-        }
-
-        [HttpGet("{id:int}")]
-        [Authorize(Roles = "Admin,Patient")]
-        public async Task<ActionResult<GetObjectDTO<GetSpecialistDTO>>> GetSpecialist(int id)
-        {
-            var specialist = await _specialistsRepository.GetAsync(id);
-
-            if (specialist == null) return ApiNotFound();
-
-            var dto = new GetSpecialistDTO(specialist);
-            return Ok(new GetObjectDTO<GetSpecialistDTO>(dto));
-        }
-
-        [HttpGet("{id:int}/evaluations")]
-        [Authorize(Roles = "Admin,Patient")]
-        public async Task<ActionResult<GetListDTO<GetEvaluationDTO>>> GetSpecialistEvaluations(int id)
-        {
-            var evaluations = (await _evaluationsRepository.GetBySpecialistId(id))
-                .Select(e => new GetEvaluationDTO(e));
-
-            return Ok(new GetListDTO<GetEvaluationDTO>(evaluations));
-        }
-
-        [Obsolete("This is a mock implementation")]
-        [HttpGet("{id:int}/visits")]
-        public ActionResult<GetListDTO<GetVisitDTO>> GetSpecialistVisits(int id)
-        {
-            var evaluations = new List<GetVisitDTO>
+            new()
             {
-                new()
-                {
-                    Id = 1,
-                    SpecialistName = "Good Doctor",
-                    PatientName = "John Doe",
-                    VisitStart = DateTime.Now.ToString("O"),
-                    VisitEnd = DateTime.Now.AddHours(1).ToString("O")
-                },
-                new()
-                {
-                    Id = 2,
-                    SpecialistName = "Good Doctor",
-                    PatientName = "John Doe",
-                    VisitStart = DateTime.Now.ToString("O"),
-                    VisitEnd = DateTime.Now.AddHours(1).ToString("O")
-                }
-            };
+                Id = 1,
+                SpecialistName = "Good Doctor",
+                PatientName = "John Doe",
+                VisitStart = DateTime.Now.ToString("O"),
+                VisitEnd = DateTime.Now.AddHours(1).ToString("O")
+            },
+            new()
+            {
+                Id = 2,
+                SpecialistName = "Good Doctor",
+                PatientName = "John Doe",
+                VisitStart = DateTime.Now.ToString("O"),
+                VisitEnd = DateTime.Now.AddHours(1).ToString("O")
+            }
+        };
 
-            var dto = new GetListDTO<GetVisitDTO>(evaluations);
+        var dto = new GetListDTO<GetVisitDTO>(evaluations);
 
-            return Ok(dto);
-        }
+        return Ok(dto);
+    }
 
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> CreateSpecialist([FromBody] CreateSpecialistDTO dto)
-        {
-            _validator.ValidateCreateSpecialistDTO(dto);
-            var specialist = new Specialist(dto);
-            await _specialistsRepository.InsertAsync(specialist);
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> CreateSpecialist([FromBody] CreateSpecialistDTO dto)
+    {
+        _validator.ValidateCreateSpecialistDTO(dto);
+        var specialist = new Specialist(dto);
+        await _specialistsRepository.InsertAsync(specialist);
 
-            return NoContent();
-        }
+        return NoContent();
+    }
 
-        [HttpPut("{id:int}")]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> EditSpecialist(
-            int id,
-            [FromBody] EditSpecialistDTO dto
-        )
-        {
-            _validator.ValidateEditSpecialistDTO(dto);
-            await _specialistsRepository.UpdateAsync(id, new Specialist(id, dto));
+    [HttpPut("{id:int}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> EditSpecialist(
+        int id,
+        [FromBody] EditSpecialistDTO dto
+    )
+    {
+        _validator.ValidateEditSpecialistDTO(dto);
+        await _specialistsRepository.UpdateAsync(id, new Specialist(id, dto));
 
-            return NoContent();
-        }
+        return NoContent();
+    }
 
-        [HttpDelete("{id:int}")]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> DeleteSpecialist(int id)
-        {
-            await _specialistsRepository.DeleteAsync(id);
+    [HttpDelete("{id:int}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeleteSpecialist(int id)
+    {
+        await _specialistsRepository.DeleteAsync(id);
 
-            return NoContent();
-        }
+        return NoContent();
     }
 }
--- Saitynas_API/Controllers/VisitsController.cs
+++ Saitynas_API/Controllers/VisitsController.cs
@@ -4,98 +4,97 @@ using Microsoft.AspNetCore.Mvc;
 using Saitynas_API.Models.DTO;
 using Saitynas_API.Models.Entities.Visit.DTO;
 
-namespace Saitynas_API.Controllers
+namespace Saitynas_API.Controllers;
+
+[Route(RoutePrefix + "/[Controller]")]
+[ApiController]
+[Produces(ApiContentType)]
+[Obsolete]
+public class VisitsController : ApiControllerBase
 {
-    [Route(RoutePrefix + "/[Controller]")]
-    [ApiController]
-    [Produces(ApiContentType)]
-    [Obsolete]
-    public class VisitsController : ApiControllerBase
+    protected override string ModelName => "visit";
+
+    [HttpGet]
+    public ActionResult<GetListDTO<GetVisitDTO>> GetVisits()
     {
-        protected override string ModelName => "visit";
-
-        [HttpGet]
-        public ActionResult<GetListDTO<GetVisitDTO>> GetVisits()
+        var evaluations = new List<GetVisitDTO>
         {
-            var evaluations = new List<GetVisitDTO>
-            {
-                new()
-                {
-                    Id = 1,
-                    SpecialistName = "Good Doctor",
-                    PatientName = "John Doe",
-                    VisitStart = DateTime.Now.ToString("O"),
-                    VisitEnd = DateTime.Now.AddHours(1).ToString("O")
-                },
-                new()
-                {
-                    Id = 2,
-                    SpecialistName = "Good Doctor",
-                    PatientName = "John Doe",
-                    VisitStart = DateTime.Now.ToString("O"),
-                    VisitEnd = DateTime.Now.AddHours(1).ToString("O")
-                }
-            };
-
-            var dto = new GetListDTO<GetVisitDTO>(evaluations);
-
-            return Ok(dto);
-        }
-
-        [HttpGet("{id:int}")]
-        public ActionResult<GetObjectDTO<GetVisitDTO>> GetVisit(int id)
-        {
-            if (id != 1) return ApiNotFound();
-
-            var dto = new GetVisitDTO
+            new()
             {
                 Id = 1,
                 SpecialistName = "Good Doctor",
                 PatientName = "John Doe",
                 VisitStart = DateTime.Now.ToString("O"),
                 VisitEnd = DateTime.Now.AddHours(1).ToString("O")
-            };
-
-            return Ok(dto);
-        }
-
-        [HttpPost]
-        public ActionResult<GetObjectDTO<GetVisitDTO>> CreateVisit([FromBody] VisitDTO dto)
-        {
-            var evaluation = new GetVisitDTO
+            },
+            new()
             {
-                Id = 10,
-                SpecialistName = dto.SpecialistName,
-                PatientName = dto.PatientName,
-                VisitStart = dto.VisitStart,
-                VisitEnd = dto.VisitEnd
-            };
+                Id = 2,
+                SpecialistName = "Good Doctor",
+                PatientName = "John Doe",
+                VisitStart = DateTime.Now.ToString("O"),
+                VisitEnd = DateTime.Now.AddHours(1).ToString("O")
+            }
+        };
 
-            return ApiCreated(new GetObjectDTO<GetVisitDTO>(evaluation));
-        }
+        var dto = new GetListDTO<GetVisitDTO>(evaluations);
 
-        [HttpPut("{id:int}")]
-        public ActionResult<GetObjectDTO<VisitDTO>> EditVisit(
-            int id,
-            [FromBody] VisitDTO dto
-        )
+        return Ok(dto);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<GetObjectDTO<GetVisitDTO>> GetVisit(int id)
+    {
+        if (id != 1) return ApiNotFound();
+
+        var dto = new GetVisitDTO
         {
-            var evaluation = new GetVisitDTO
-            {
-                Id = 10,
-                SpecialistName = dto.SpecialistName,
-                PatientName = dto.PatientName,
-                VisitStart = dto.VisitStart,
-                VisitEnd = dto.VisitEnd
-            };
+            Id = 1,
+            SpecialistName = "Good Doctor",
+            PatientName = "John Doe",
+            VisitStart = DateTime.Now.ToString("O"),
+            VisitEnd = DateTime.Now.AddHours(1).ToString("O")
+        };
 
-            return Ok(new GetObjectDTO<GetVisitDTO>(evaluation));
-        }
+        return Ok(dto);
+    }
 
-        [HttpDelete("{id:int}")]
-        public IActionResult DeleteVisit(int id)
+    [HttpPost]
+    public ActionResult<GetObjectDTO<GetVisitDTO>> CreateVisit([FromBody] VisitDTO dto)
+    {
+        var evaluation = new GetVisitDTO
         {
-            return NoContent();
-        }
+            Id = 10,
+            SpecialistName = dto.SpecialistName,
+            PatientName = dto.PatientName,
+            VisitStart = dto.VisitStart,
+            VisitEnd = dto.VisitEnd
+        };
+
+        return ApiCreated(new GetObjectDTO<GetVisitDTO>(evaluation));
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult<GetObjectDTO<VisitDTO>> EditVisit(
+        int id,
+        [FromBody] VisitDTO dto
+    )
+    {
+        var evaluation = new GetVisitDTO
+        {
+            Id = 10,
+            SpecialistName = dto.SpecialistName,
+            PatientName = dto.PatientName,
+            VisitStart = dto.VisitStart,
+            VisitEnd = dto.VisitEnd
+        };
+
+        return Ok(new GetObjectDTO<GetVisitDTO>(evaluation));
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteVisit(int id)
+    {
+        return NoContent();
     }
 }
