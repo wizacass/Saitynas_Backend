aaa0ec1e9a4e527cf4b9de3663eb0ae24c554d3d
Vytenis Petrauskas <visak.pet0@gmail.com>
2022-01-16 20:14:35 +0200

    Apply file-scoped namespaces to exceptions

--- Saitynas_API/Exceptions/DTOValidationException.cs
+++ Saitynas_API/Exceptions/DTOValidationException.cs
@@ -1,14 +1,13 @@
 using System;
 
-namespace Saitynas_API.Exceptions
-{
-    public class DTOValidationException : Exception
-    {
-        public string Parameter { get; }
+namespace Saitynas_API.Exceptions;
 
-        public DTOValidationException(string message, string parameter = null) : base(message)
-        {
-            Parameter = parameter ?? "";
-        }
+public class DTOValidationException : Exception
+{
+    public DTOValidationException(string message, string parameter = null) : base(message)
+    {
+        Parameter = parameter ?? "";
     }
+
+    public string Parameter { get; }
 }
